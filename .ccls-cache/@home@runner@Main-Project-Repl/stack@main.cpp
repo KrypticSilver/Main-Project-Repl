bcfccfd8b99b963a61a29132051fc821c4defe5e
KrypticSilver <53061712+KrypticSilver@users.noreply.github.com>
2023-04-16 01:27:20 +0000

    restructured

---
+++ .ccls-cache/@home@runner@Main-Project-Repl/stack@main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void stackPush(vector<int> &myStack, int item) {
+  myStack.push_back(item);
+}
+
+void stackPop(vector<int> &myStack) {
+  
+}
+
+int main() {
+  vector<int> stack;
+
+  
+}
---
+++ distance-calculator/test.cpp
@@ -0,0 +1,9 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main1() {
+  cout << pow(5, 3);
+  
+}
